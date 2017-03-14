3dacd8c67d3f4f095e1fb5653d20ebb4dcb5e96e
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-03-14 14:20:21 -0400

    User authentication working as intended.  Need errors to flash when incorrect login information is provided.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/register', to: 'users#new'
   get '/signin', to: 'sessions#new'
-  post '/sigin', to: 'sessions#create'
+  post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
 
   resources :users
