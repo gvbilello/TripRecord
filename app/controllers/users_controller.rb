c92b1f0595eed797db8abd7d8055db641043a12b
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-03-14 13:50:00 -0400

    Users Controller Added.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			flash[:success] = 'Account Successfully Created'
+			redirect_to user
+		else
+			@errors = user.errors.full_messages
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find_by(id: session[:user_id])
+	end
+
+	def update
+		@user = User.find_by(id: session[:user_id])
+		if @user.update_attributes(user_params)
+			flash[:success] = 'Account Information Successfully Updated'
+			redirect_to @user
+		else
+			@errors = user.errors.full_messages
+			render 'edit'
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :email, :password, :password_confirmation)
+		end
+
+		def logged_in_user
+			unless logged_in?
+				store_location
+				flash[:danger] = 'Please Log In'
+				redirect_to signin_url
+			end
+		end
+
+		def correct_user
+			user = User.find_by(id: params[:id])
+			redirect_to(root_url) unless current_user?(user)
+		end
+
+	end
