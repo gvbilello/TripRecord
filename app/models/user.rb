5b03b2af067ec0dfa9d8967fee028e81b50eeb25
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-03-14 14:06:26 -0400

    Added views, routes, and static controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
-	
+
 	has_many :trips
+	validates_presence_of :username, :email
+	validates_uniqueness_of :username, :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/register', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/sigin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  resources :users
+  root 'static#index'
 end
