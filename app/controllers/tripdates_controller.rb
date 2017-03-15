c975309f9e64c43f60ba0a663b3b6221e962427c
Gregory V. Bilello <gregory.bilello@gmail.com>
2017-03-15 09:54:50 -0400

    Added controllers for other models.

---
+++ app/controllers/tripdates_controller.rb
@@ -0,0 +1,23 @@
+class TripDatesController < ApplicationController
+
+	def show
+	end
+	
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+end
